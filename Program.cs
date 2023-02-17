afa7ba23e4ba14623d6f21b11564b8899350b04d
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-17 17:42:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        int n, i;
+
+        Console.Write("Quantos numeros voce vai digitar? ");
+        n = int.Parse(Console.ReadLine());
+
+        double[] num = new double[n];
+
+        for (i = 0; i < n; i++)
+        {
+            Console.Write("Digite um numero: ");
+            num[i] = double.Parse(Console.ReadLine(), CI);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("NUMERO DIGITADOS: ");
+
+        for (i = 0; i < n; i++) 
+        {
+            Console.WriteLine(num[i].ToString("F1", CI);
+        }
+    }
+}
