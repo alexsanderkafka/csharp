62395cd623a1e0277255001f421c84d5ec55762a
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-17 17:47:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b;
+
+        Console.WriteLine("Digite dois numeros: ");
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+
+        while (a != b)
+        {
+            if (a > b) 
+            {
+                Console.WriteLine("DECRESCENTE!");
+
+            }
+            else
+            {
+                Console.WriteLine("CRESCENTE!");
+            }
+
+            Console.WriteLine("Digite outro dois numeros: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+        }
+    }
+}
