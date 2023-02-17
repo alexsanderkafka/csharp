5406a363e9721748903488fea268d3161e7a8038
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-17 17:46:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // problema dentro ou fora
+        // dentro números que estao no intervalo entre 10 e 20
+        // fora numeros que estão fora desse intervalo
+
+        int x, n, dentro, fora;
+
+        Console.Write("Quantos numeros voce vai digitar? ");
+        n = int.Parse(Console.ReadLine());
+
+        dentro = 0;
+        fora = 0;
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Digite um numero: ");
+            x = int.Parse(Console.ReadLine());
+
+            if (x >= 10 && x <= 20)
+            {
+                dentro++;
+            }
+            else
+            {
+                fora++;
+            }
+        }
+
+        Console.WriteLine(dentro + " DENTRO");
+        Console.WriteLine(fora + " FORA");
+
+    }
+}
