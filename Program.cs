6ed2f2229d6a9679833a887491112d634a332b25
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-17 17:48:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int linha, coluna, i, j;
+
+        Console.Write("Quantas linhas vai ter a matriz? ");
+        linha = int.Parse(Console.ReadLine());
+        Console.Write("Quantas colunas vai ter a matriz? ");
+        coluna = int.Parse(Console.ReadLine());
+
+        int[,] mat = new int[linha, coluna];
+
+        for (i = 0; i < linha; i++)
+        {
+            for (j = 0; j < coluna; j++)
+            {
+                Console.Write("Elemento [" + i + " , " + j + "]: ");
+                mat[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("MATRIZ DIGITADA: ");
+        for (i = 0; i < linha; i++)
+        {
+            for (j = 0; j < coluna; j++)
+            {
+                Console.Write(mat[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+
+
+    }
+}
