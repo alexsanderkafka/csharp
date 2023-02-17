82015e302e50ae90a48fd7c8ed0df78376b432b8
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-17 17:45:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, i;
+
+        Console.Write("Quantos numeros voce vai digitar: ");
+        n = int.Parse(Console.ReadLine());
+
+        int[] vet = new int[n];
+
+        for (i = 0; i < n; i++) 
+        {
+            Console.Write("Digite um numero: ");
+            vet[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("NUMEROS NEGATIVOS: ");
+        for (i = 0; i < n; i++) 
+        {
+            if (vet[i] < 0) 
+            {
+                Console.WriteLine(vet[i]);
+            }
+        }
+
+    }
+}
