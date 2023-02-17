80449ca7d8e0cc4d32aaccdd491812f65406c749
Alexsander Kafka <123211650+alexsanderkafka@users.noreply.github.com>
2023-02-17 17:44:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture; 
+
+        double altura, base1, area, perimetro, diagonal;
+
+        Console.Write("Base do retangulo: ");
+        base1 = double.Parse(Console.ReadLine(), CI);
+
+        Console.Write("Altura do retangulo: ");
+        altura = double.Parse(Console.ReadLine(), CI);
+
+        Console.WriteLine();
+
+        area = base1 * altura;
+        Console.WriteLine("AREA = " + area.ToString("F4", CI));
+
+        perimetro = 2 * (altura + base1);
+        Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CI));
+
+        diagonal = Math.Sqrt(Math.Pow(base1, 2) + Math.Pow(altura, 2));
+        Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CI));
+
+    }
+}
